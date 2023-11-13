9b5e3d4a1edf9c7f6f61d70570eda9e692b4a294
Lois Rabergeau <lois.rabergeau@etu.univ-poitiers.fr>
2023-11-13 16:39:28 +0100

    Fix unit test for App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './style.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
+import routes from './routes';
 
-  const h1Element = screen.getByRole('heading', { level: 1, name: "Marvel App" });
-  expect(h1Element).toBeInTheDocument();
-});
+// Create a router that uses the client side history strategy for
+const router = createBrowserRouter(routes)
+
+function App() {
+
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
